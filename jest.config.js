a5553a26a13a73632276cf146905243df846ecd2
Jonathan Luiz <luiz_jal@yahoo.com.br>
2024-04-11 21:09:09 +0000

    update Jest to connect in database

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 // Permite que o JEST consiga rodar os testes no banco como o NEXT,
 // por padrao ele nao possui este recurso.
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+});
+
 const nextJest = require("next/jest");
 
-const createJestConfig = nextJest();
+const createJestConfig = nextJest({
+  dir: ".",
+});
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
 });
