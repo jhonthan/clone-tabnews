ff714c718aa7b67d725740cc281307390a75dd79
Jonathan Luiz <luiz_jal@yahoo.com.br>
2025-01-07 15:48:23 +0000

    build: update status page

--- pages/status/index.js
+++ pages/status/index.js
@@ -11,6 +11,7 @@ export default function StatusPage() {
     <>
       <h1>Status</h1>
       <UpdatedAt />
+      <DatabaseStatus />
     </>
   );
 }
@@ -20,11 +21,40 @@ function UpdatedAt() {
     refreshInterval: 2000,
   });
 
-  let UpdatedAtText = "Carregando...";
+  let updatedAtText = "Carregando...";
 
-  if(!isLoading && data) {
-    UpdatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
   }
 
-  return <div>Ultima atualizacao: {UpdatedAtText}</div>;
+  return <div>Última atualização: {updatedAtText}</div>;
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <div>Versão: {data.dependencies.database.version}</div>
+        <div>
+          Conexões abertas: {data.dependencies.database.opened_connections}
+        </div>
+        <div>
+          Conexões máximas: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Database</h2>
+      <div>{databaseStatusInformation}</div>
+    </>
+  );
 }
