2cfad376f15ca51e79f43546fb1b53237bf9bfe7
Jonathan Luiz <luiz_jal@yahoo.com.br>
2025-02-21 15:11:28 +0000

    feat: hide `migrator` logs

--- models/migrator.js
+++ models/migrator.js
@@ -6,7 +6,7 @@ const defaultMigrartionOptions = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
